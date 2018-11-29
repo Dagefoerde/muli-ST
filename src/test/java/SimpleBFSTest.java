f4b1ba193189dc0574202ba033f50f8f0cd8f60e
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2018-11-29 11:07:36 +0100

    Test AssignmentCoin

--- src/test/java/SimpleBFSTest.java
+++ src/test/java/SimpleBFSTest.java
@@ -31,4 +31,26 @@ class SimpleBFSTest {
         assertTrue(trail.isEmpty());
     }
 
+    @Test
+    public void assignmentCoinTest() throws NoSuchFieldException, IllegalAccessException {
+        TestableLogicVM vm = new TestableLogicVM();
+        vm.setProgram(new examples.AssignmentCoin());
+        ST<Object> tree = new STProxy<>(0, null);
+        List<Object> leaves = TreeBFSIterator.stream(tree, vm).limit(4).collect(Collectors.toList());
+
+        Stream.of(leaves).forEach(System.out::println);
+        STDemo.printDFS(tree, 0, vm);
+
+        // We intentionally requested up to 4 using `limit()` but the program only has 2 solutions.
+        assertEquals(2, leaves.size());
+
+        assertIterableEquals(leaves, Arrays.asList(new Integer[]{0, 1}));
+
+        HashMap<String, Integer> heap = vm.inspectHeap();
+        assertTrue(heap.isEmpty());
+
+        LinkedList<TrailElement> trail = vm.inspectTrail();
+        assertTrue(trail.isEmpty());
+    }
+
 }
--- src/test/java/SimpleDFSTest.java
+++ src/test/java/SimpleDFSTest.java
@@ -13,8 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleDFSTest {
     @Test
@@ -28,6 +27,28 @@ class SimpleDFSTest {
         STDemo.printDFS(tree, 0, vm);
 
         assertIterableEquals(leaves, Arrays.asList(new String[]{"coin || False", "coin && True"}));
+
+        HashMap<String, Integer> heap = vm.inspectHeap();
+        assertTrue(heap.isEmpty());
+
+        LinkedList<TrailElement> trail = vm.inspectTrail();
+        assertTrue(trail.isEmpty());
+    }
+
+    @Test
+    public void assignmentCoinTest() throws NoSuchFieldException, IllegalAccessException {
+        TestableLogicVM vm = new TestableLogicVM();
+        vm.setProgram(new examples.AssignmentCoin());
+        ST<Object> tree = new STProxy<>(0, null);
+        List<Object> leaves = TreeDFSIterator.stream(tree, vm).limit(4).collect(Collectors.toList());
+
+        Stream.of(leaves).forEach(System.out::println);
+        STDemo.printDFS(tree, 0, vm);
+
+        // We intentionally requested up to 4 using `limit()` but the program only has 2 solutions.
+        assertEquals(2, leaves.size());
+
+        assertIterableEquals(leaves, Arrays.asList(new Integer[]{0, 1}));
 
         HashMap<String, Integer> heap = vm.inspectHeap();
         assertTrue(heap.isEmpty());
